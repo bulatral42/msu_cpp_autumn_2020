a8b3a333b177791bfcdd4d62a18a2a03af15916f
Bulat Valiahmetov <valiahmetovbulat@mail.ru>
2020-12-15 16:29:00 +0300

    done

---
+++ 09/tests.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <exception>
+#include <cassert>
+#include <cstdlib>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include "threadsort.hpp"
+
+
+constexpr size_t N = 1024;
+
+
+void print_test_number()
+{
+    static int n{1};
+    std::cout << "Test " << n << ": " << std::endl;
+    ++n;
+}
+
+/* Test 1 */
+/* 2 threads */
+void test_th2() 
+{
+    print_test_number();
+    try {
+        std::string filename = "check1.bin";
+        std::vector<uint64_t> vec;
+        vec.reserve(N);
+        std::ofstream file("check1.bin", std::ios::binary);
+        for (size_t i = 0; i < N; ++i) {
+            uint64_t tmp = std::rand();
+            vec.push_back(tmp);
+            file.write((char *)&tmp, sizeof(tmp));
+        }
+        file.close();
+
+        threadsort("check1.bin", 2);
+        std::sort(vec.begin(), vec.end());
+        
+        std::ifstream file2("check1.bin_res.bin", std::ios::binary);
+        for (size_t i = 0; i < N; ++i) {
+            uint64_t tmp;
+            file2.read((char *)&tmp, sizeof(tmp));
+            if (vec[i] != tmp) {
+                std::cout << "Dismath in index " << i << std::endl;
+                std::cout << vec[i] << " != " << tmp << std::endl;
+                assert(0 && "Wrong sorting");
+            }
+        }
+    } catch (const std::exception &ex) {
+        std::cout << ex.what() << std::endl;
+        assert(0 && "std::exception is caught");
+    }
+    std::cout << "Report: OK" << std::endl;
+}
+
+
+/* Test 2 */
+/* 8 threads */
+void test_th8() 
+{
+    print_test_number();
+    try {
+        std::string filename = "check2.bin";
+        std::vector<uint64_t> vec;
+        vec.reserve(N);
+        std::ofstream file("check2.bin", std::ios::binary);
+        for (size_t i = 0; i < N; ++i) {
+            uint64_t tmp = std::rand();
+            file.write((char *)&tmp, sizeof(tmp));
+            vec.push_back(tmp);
+        }
+        file.close();
+
+        threadsort("check2.bin", 8);
+        
+        std::sort(vec.begin(), vec.end());
+        std::ifstream file2("check2.bin_res.bin", std::ios::binary);
+        for (size_t i = 0; i < N; ++i) {
+            uint64_t tmp;
+            file2.read((char *)&tmp, sizeof(tmp));
+            if (vec[i] != tmp) {
+                std::cout << "Dismath in index " << i << std::endl;
+                std::cout << vec[i] << " != " << tmp << std::endl;
+                assert(0 && "Wrong sorting");
+            }
+        }
+    } catch (const std::exception &ex) {
+        std::cout << ex.what() << std::endl;
+        assert(0 && "std::exception is caught");
+    }
+    std::cout << "Report: OK" << std::endl;
+}
+
+
+int main() 
+{  
+    test_th2();
+    test_th8();
+    return 0;
+}
---
+++ 09/threadsort.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <fstream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+
+
+
+constexpr size_t MAX_CHUNKS = 16;
+constexpr size_t CHUNK_CNT = 4;
+
+constexpr size_t CHUNK_SIZE = CHUNK_CNT * sizeof(uint64_t);
+
+constexpr size_t BUF_SIZE = CHUNK_SIZE * MAX_CHUNKS;
+constexpr size_t BUF_CNT = CHUNK_CNT * MAX_CHUNKS;
+
+
+void merge_files(const std::vector<std::string> &fnames, 
+                 const std::string &res_fname);
+void threadsort_worker(std::string thr_name, std::ifstream &file,
+                       std::mutex &read_mutex);
+std::string threadsort(const char *filename, size_t n_threads);
